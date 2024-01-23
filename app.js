1f5f942cb371bf1ed8bb3265b29073c591734bda
Anthony de Hoog <37657@ma-web.nl>
2024-01-23 14:19:49 +0100

    Meesterwerk afgemaakt

--- app.js
+++ app.js
@@ -9,5 +9,20 @@ function dark() {
     element.classList.toggle("dark-mode");
   }
 
+  const button = document.getElementById("js--tickets");
+
+  const navigation = document.getElementById("js--nav");
+  
+  const body = document.getElementById("js--body");
+
+  const Top = document.getElementById("js--top");
+  
+  button.onclick = function() {
+      navigation.style.visibility = "visible";
+      navigation.style.opacity = 1;
+      body.style.overflow = "hidden";
+      Top.style.visibility = "hidden";
+  }
+
 let app = new App();
 app.runApplication();
